49682d9f585616492df1eef4f0ebf0ac480a8ca0
Termininja <termininja@mail.bg>
2017-09-13 20:45:36 +0300

    Helper

---
+++ Helper/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helper")]
+[assembly: AssemblyCopyright("Termininja")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e942442e-ece8-4c59-8163-d78c0fc5e4c6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
